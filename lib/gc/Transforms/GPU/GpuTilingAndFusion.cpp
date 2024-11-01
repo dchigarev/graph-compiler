def5e269cc2b03c57bd2a4dcd079c23ebb51a808
Andrey Pavlenko <andrey.a.pavlenko@gmail.com>
2024-11-01 19:10:34 +0100

    rework2

--- lib/gc/Transforms/GPU/GpuTilingAndFusion.cpp
+++ lib/gc/Transforms/GPU/GpuTilingAndFusion.cpp
@@ -7,7 +7,6 @@
 //===----------------------------------------------------------------------===//
 
 #include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
-#include "mlir/Conversion/LLVMCommon/Pattern.h"
 #include "mlir/Conversion/Passes.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Affine/Utils.h"
@@ -21,10 +20,10 @@
 #include "mlir/Transforms/RegionUtils.h"
 
 #include "./GpuUtils.h"
-#include "gc/Utils/Log.h"
 
 using namespace mlir;
 using namespace mlir::gc;
+using namespace mlir::scf;
 
 namespace mlir::gc {
 #define GEN_PASS_DECL_GPUTILINGANDFUSION
@@ -44,67 +43,97 @@ struct GpuTilingAndFusion final
       : GpuPass(), GpuTilingAndFusionBase(opts) {}
 
   void runOnOperation() override {
-    IRRewriter rewriter(&getContext());
-    scf::SCFTileAndFuseOptions opts;
-    auto numEus = getNumEus(rewriter);
-    auto numEusPerSlice = getNumEusPerSlice(rewriter);
-    auto numThreadsPerEu = getNumThreadsPerEu(rewriter);
-    auto cacheSize = getCacheSize(rewriter);
-    auto vectorWidth = getVectorWidth(rewriter);
+    auto fn = getOperation();
+    if (fn.isExternal()) {
+      return;
+    }
+
+    OpRewriter rw(fn);
+    tileAndFuseLinalgOps(rw, fn);
+    tileForallOps(rw, fn);
+    if (failed(simplifyRegions(rw, fn->getRegions()))) {
+      // Not simplified
+    }
+  }
+
+private:
+  void tileAndFuseLinalgOps(OpRewriter &rw, func::FuncOp &fn) {
+    auto numEus = getNumEus(rw);
+    auto numEusPerSlice = getNumEusPerSlice(rw);
+    auto numThreadsPerEu = getNumThreadsPerEu(rw);
+    auto cacheSize = getCacheSize(rw);
+    auto vectorWidth = getVectorWidth(rw);
     auto cachePerThread =
         std::max(cacheSize / numEusPerSlice / numThreadsPerEu, vectorWidth);
+    scf::SCFTileAndFuseOptions opts;
     opts.tilingOptions.setTileSizeComputationFunction(
-        [cachePerThread, vectorWidth, numThreads = numEus * numThreadsPerEu](
+        [&rw, cachePerThread, vectorWidth,
+         numThreads = numEus * numThreadsPerEu](
             OpBuilder &builder, Operation *op) -> SmallVector<OpFoldResult> {
           auto ti = dyn_cast<TilingInterface>(op);
           if (!ti) {
             return {};
           }
 
+          rw.loc = op->getLoc();
+          rw.setInsertionPoint(op);
           auto itTypes = ti.getLoopIteratorTypes();
           auto itDomains = ti.getIterationDomain(builder);
           assert(itTypes.size() == itDomains.size());
 
-          SmallVector<int64_t> tiles;
+          SmallVector<int64_t> sizes;
+          int64_t maxSize = 0;
           int64_t numIterations = 1;
           for (auto [t, r] : zip(itTypes, itDomains)) {
             if (t == utils::IteratorType::parallel) {
               if (auto v = getConstantIntValue(r.size)) {
                 numIterations *= *v;
-                tiles.emplace_back(*v);
+                sizes.emplace_back(*v);
+                maxSize = std::max(maxSize, *v);
               } else {
-                return computeDynamicTiles(builder, ti, numThreads,
-                                           cachePerThread);
+                return computeDynamicTiles(rw, ti, numThreads, cachePerThread);
               }
             }
           }
 
-          if (tiles.empty()) {
-            return {};
-          }
-
+          assert(!sizes.empty());
           auto elementSize = getElementSize(op);
           auto sizePerThread = numIterations / numThreads * elementSize;
           auto totalSize = std::max(sizePerThread, cachePerThread);
           totalSize = std::max(totalSize / elementSize, 64L);
 
           // If the operation could be lowered to XeGPU, make the tiles
-          // proportional to the vector width.
+          // multiple of the vector width.
           if (canLowerToXeGPU(op)) {
             totalSize = std::max(totalSize / vectorWidth, 1L) * vectorWidth;
           }
 
+          SmallVector<int64_t> tiles = sizes;
           adjustTiles(totalSize, tiles);
+
+          // If the tiles are not less than the sizes, split the largest tile
+          // into two to avoid loops elimination by the canonicalizer pass.
+          if (auto pairs = zip(sizes, tiles);
+              std::all_of(pairs.begin(), pairs.end(), [](auto p) {
+                return std::get<0>(p) <= std::get<1>(p);
+              })) {
+            for (auto &t : tiles) {
+              if (t >= maxSize) {
+                t = std::max(1L, floorPow2(maxSize / 2));
+                break;
+              }
+            }
+          }
 
           unsigned counter = 0;
           SmallVector<OpFoldResult> result;
           result.reserve(itDomains.size());
 
           for (auto [t, r] : zip(itTypes, itDomains)) {
-            if (t != utils::IteratorType::parallel) {
-              result.emplace_back(builder.getIndexAttr(0));
+            if (t == utils::IteratorType::parallel) {
+              result.emplace_back(rw.getConstant(tiles[counter++]));
             } else {
-              result.emplace_back(builder.getIndexAttr(tiles[counter++]));
+              result.emplace_back(rw.getConstant(0));
             }
           }
 
@@ -126,16 +155,8 @@ struct GpuTilingAndFusion final
         });
     opts.tilingOptions.setLoopType(scf::SCFTilingOptions::LoopType::ForallOp);
 
-    auto fn = getOperation();
-    tileAndFuse(fn, rewriter, opts);
-  }
-
-private:
-  static void tileAndFuse(Operation *op, RewriterBase &rewriter,
-                          const scf::SCFTileAndFuseOptions &opts) {
-    for (auto ti = findTi(op); ti; ti = findTi(op)) {
-      auto result =
-          scf::tileConsumerAndFuseProducersUsingSCF(rewriter, *ti, opts);
+    for (auto ti = findTi(rw, fn); ti; ti = findTi(rw, fn)) {
+      auto result = scf::tileConsumerAndFuseProducersUsingSCF(rw, *ti, opts);
 
       if (failed(result)) {
         ti->emitError() << "Failed to tile and fuse using SCF";
@@ -145,49 +166,39 @@ private:
       SmallVector<Operation *> opsToReplace{ti->getOperation()};
       append_range(opsToReplace, result->fusedProducers);
       for (Operation *toReplace : opsToReplace) {
-        if (toReplace->getParentOp() == nullptr) {
-          continue;
-        }
-
         for (OpResult res : toReplace->getResults()) {
           if (auto repl = result->replacements.lookup(res)) {
-            rewriter.replaceAllUsesWith(res, repl);
+            rw.replaceAllUsesWith(res, repl);
           }
         }
+      }
 
-        if (failed(simplifyRegions(rewriter, op->getRegions()))) {
-          gcLogD("Failed to simplify regions");
-        }
-
-        if (toReplace->getParentOp() == nullptr) {
-          continue;
-        }
-
-        // For some reason (probably a bug?) the operation could be
-        // referenced by a dead code inside the replacement, that prevents
-        // this operation from being erased. Erasing the dead code first.
-        for (auto u : toReplace->getUsers()) {
-          if (u->use_empty()) {
-            rewriter.eraseOp(u);
-          }
-        }
-
-        if (toReplace->use_empty()) {
-          rewriter.eraseOp(toReplace);
-        } else {
-          gcLogE("Unable to erase operation!");
-        }
+      if (failed(simplifyRegions(rw, fn->getRegions()))) {
+        // Not simplified
       }
     }
   }
 
-  static std::optional<TilingInterface> findTi(Operation *op) {
+  static std::optional<TilingInterface> findTi(OpBuilder &b, Operation *op) {
     std::optional<TilingInterface> last;
     op->walk<WalkOrder::PreOrder>([&](linalg::LinalgOp linalgOp) {
       if (linalgOp.hasOnlyProjectedPermutations() &&
           !linalgOp->getParentOfType<scf::ForallOp>()) {
         if (auto ti = dyn_cast<TilingInterface>(linalgOp.getOperation())) {
-          last = ti;
+          int64_t numTiles = 0;
+          int64_t numIterations = 1;
+          for (auto [t, r] :
+               zip(ti.getLoopIteratorTypes(), ti.getIterationDomain(b))) {
+            if (t == utils::IteratorType::parallel) {
+              numTiles++;
+              if (auto v = getConstantIntValue(r.size)) {
+                numIterations *= *v;
+              }
+            }
+          }
+          if (numTiles > 0 && numIterations >= 32) {
+            last = ti;
+          }
         }
       }
       return WalkResult::skip();
@@ -195,15 +206,16 @@ private:
     return last;
   }
 
-  static SmallVector<OpFoldResult> computeDynamicTiles(OpBuilder &builder,
+  static SmallVector<OpFoldResult> computeDynamicTiles(OpRewriter &rw,
                                                        TilingInterface ti,
                                                        int64_t numThreads,
                                                        int64_t cachePerThread) {
     auto itTypes = ti.getLoopIteratorTypes();
-    auto itDomains = ti.getIterationDomain(builder);
+    auto itDomains = ti.getIterationDomain(rw);
     assert(itTypes.size() == itDomains.size());
+    rw.loc = ti.getLoc();
+    rw.setInsertionPoint(ti.getOperation());
 
-    auto loc = ti.getLoc();
     Value dynamicSize;
     auto staticSize = getElementSize(ti.getOperation());
     unsigned loopCount = 0;
@@ -216,8 +228,8 @@ private:
       if (auto v = getConstantIntValue(r.size)) {
         staticSize *= *v;
       } else if (dynamicSize) {
-        dynamicSize = builder.create<arith::MulIOp>(loc, dynamicSize,
-                                                    r.size.get<Value>());
+        dynamicSize =
+            rw.create<arith::MulIOp>(dynamicSize, r.size.get<Value>());
       } else {
         dynamicSize = r.size.get<Value>();
       }
@@ -226,54 +238,47 @@ private:
     assert(loopCount);
     assert(dynamicSize);
     if (staticSize > 1) {
-      dynamicSize = builder.create<arith::MulIOp>(
-          loc, dynamicSize,
-          builder.create<arith::ConstantIndexOp>(loc, staticSize));
+      dynamicSize =
+          rw.create<arith::MulIOp>(dynamicSize, rw.getConstant(staticSize));
     }
-    auto i64Type = builder.getI64Type();
-    dynamicSize = builder.create<arith::UIToFPOp>(
-        loc, builder.getF64Type(),
-        builder.create<arith::IndexCastOp>(loc, i64Type, dynamicSize));
+    auto i64Type = rw.getI64Type();
+    dynamicSize = rw.create<arith::UIToFPOp>(
+        rw.getF64Type(), rw.create<arith::IndexCastOp>(i64Type, dynamicSize));
 
     // TODO: Call the adjustTiles() function for the tiles calculation.
 
-    auto nt = builder.create<arith::ConstantFloatOp>(
-        loc, APFloat(static_cast<double>(numThreads)), builder.getF64Type());
-    auto cpt = builder.create<arith::ConstantFloatOp>(
-        loc, APFloat(static_cast<double>(cachePerThread)),
-        builder.getF64Type());
-    Value totalSize = builder.create<arith::MaximumFOp>(
-        loc, builder.getF64Type(),
-        builder.create<arith::DivFOp>(loc, dynamicSize, nt), cpt);
-    auto pow = builder.create<arith::ConstantFloatOp>(
-        loc, APFloat(1.0 / loopCount), builder.getF64Type());
+    auto nt = rw.create<arith::ConstantFloatOp>(
+        APFloat(static_cast<double>(numThreads)), rw.getF64Type());
+    auto cpt = rw.create<arith::ConstantFloatOp>(
+        APFloat(static_cast<double>(cachePerThread)), rw.getF64Type());
+    Value totalSize = rw.create<arith::MaximumFOp>(
+        rw.getF64Type(), rw.create<arith::DivFOp>(dynamicSize, nt), cpt);
+    auto pow = rw.create<arith::ConstantFloatOp>(APFloat(1.0 / loopCount),
+                                                 rw.getF64Type());
     // The average tile size is totalSize^(1 / loopCount)
-    Value avgTileSize = builder.create<math::PowFOp>(loc, totalSize, pow);
-    avgTileSize = builder.create<arith::MaximumFOp>(
-        loc, builder.getF64Type(),
-        builder.create<arith::ConstantFloatOp>(loc, APFloat(1.0),
-                                               builder.getF64Type()),
+    Value avgTileSize = rw.create<math::PowFOp>(totalSize, pow);
+    avgTileSize = rw.create<arith::MaximumFOp>(
+        rw.getF64Type(),
+        rw.create<arith::ConstantFloatOp>(APFloat(1.0), rw.getF64Type()),
         avgTileSize);
-    avgTileSize = builder.create<arith::FPToSIOp>(loc, i64Type, avgTileSize);
+    avgTileSize = rw.create<arith::FPToSIOp>(i64Type, avgTileSize);
 
     SmallVector<OpFoldResult> tiles;
     tiles.reserve(itDomains.size());
 
     for (auto [t, r] : zip(itTypes, itDomains)) {
       if (t != utils::IteratorType::parallel) {
-        tiles.emplace_back(builder.getIndexAttr(1));
+        tiles.emplace_back(rw.getIndexAttr(1));
       } else {
         Value value;
         if (auto v = getConstantIntValue(r.size)) {
-          value = builder.create<arith::ConstantIntOp>(loc, *v, i64Type);
+          value = rw.create<arith::ConstantIntOp>(*v, i64Type);
         } else {
-          value = builder.create<arith::IndexCastOp>(loc, i64Type,
-                                                     r.size.get<Value>());
+          value = rw.create<arith::IndexCastOp>(i64Type, r.size.get<Value>());
         }
-        value =
-            builder.create<arith::MinSIOp>(loc, i64Type, value, avgTileSize);
-        tiles.emplace_back(builder.create<arith::IndexCastOp>(
-            loc, builder.getIndexType(), value));
+        value = rw.create<arith::MinSIOp>(i64Type, value, avgTileSize);
+        tiles.emplace_back(
+            rw.create<arith::IndexCastOp>(rw.getIndexType(), value));
       }
     }
 
@@ -281,16 +286,15 @@ private:
   }
 
   static int64_t getElementSize(Operation *op) {
-    int64_t elementSize = 1;
     if (auto linalgOp = dyn_cast<linalg::LinalgOp>(op)) {
       if (auto inits = linalgOp.getDpsInits(); !inits.empty()) {
         if (auto t = getElementTypeOrSelf(inits[0].getType());
             t.isIntOrFloat()) {
-          elementSize = t.getIntOrFloatBitWidth() / 8;
+          return std::max(1L, t.getIntOrFloatBitWidth() / 8L);
         }
       }
     }
-    return elementSize;
+    return 1L;
   }
 
   // TODO: Add more checks
@@ -337,6 +341,131 @@ private:
     }
 
     return true;
+  }
+
+  void tileForallOps(OpRewriter &rw, func::FuncOp &fn) {
+    auto wgSize = getWorkGroupSize(rw);
+    fn.walk<WalkOrder::PreOrder>([&rw, wgSize](scf::ForallOp loop) {
+      if (!loop->getParentOfType<scf::ForallOp>()) {
+        tileForallOp(rw, loop, wgSize);
+      }
+      return WalkResult::skip();
+    });
+  }
+
+  static void tileForallOp(OpRewriter &rw, ForallOp op, int64_t wgSize) {
+    rw.loc = op.getLoc();
+    rw.setInsertionPoint(op);
+    OpFoldResult zero{rw.getConstant(0)};
+    OpFoldResult one{rw.getConstant(1)};
+    auto innerSteps = op.getMixedStep();
+    auto outerBounds = op.getMixedUpperBound();
+    SmallVector<OpFoldResult> outerSteps;
+    auto count = innerSteps.size();
+
+    { // Calculate outer steps
+      SmallVector<int64_t> tiles;
+      tiles.reserve(count);
+      for (auto s : innerSteps) {
+        if (auto v = getConstantIntValue(s)) {
+          tiles.emplace_back(*v);
+        } else {
+          // TODO: Add support for dynamic sizes
+          tiles.emplace_back(32);
+        }
+      }
+      adjustTiles(wgSize, tiles);
+      outerSteps.reserve(count);
+      for (auto [s, b, t] : zip(innerSteps, outerBounds, tiles)) {
+        if (auto sv = getConstantIntValue(s)) {
+          auto step = *sv * t;
+          if (auto bv = getConstantIntValue(b)) {
+            step = std::min(step, *bv);
+          }
+          outerSteps.emplace_back(rw.getConstant(step));
+        } else {
+          outerSteps.emplace_back(
+              rw.create<arith::MulIOp>(s.get<Value>(), rw.getConstant(t)));
+        }
+      }
+    }
+
+    auto outerLoop =
+        rw.create<ForallOp>(op.getMixedLowerBound(), outerBounds, outerSteps,
+                            op.getOutputs(), std::nullopt);
+    rw.setInsertionPointToStart(outerLoop.getBody());
+    SmallVector<OpFoldResult> innerBounds;
+    SmallVector<Range> ranges;
+
+    {
+      auto idxType = rw.getIndexType();
+      auto ctx = rw.getContext();
+      auto minMap = AffineMap::get(
+          /*dimCount=*/3, /*symbolCount=*/0,
+          {getAffineDimExpr(0, ctx),
+           getAffineDimExpr(1, ctx) - getAffineDimExpr(2, ctx)},
+          rw.getContext());
+      innerBounds.reserve(count);
+      ranges.reserve(count);
+      for (auto [i, u, s] : zip(outerLoop.getInductionVars(),
+                                outerLoop.getMixedUpperBound(), outerSteps)) {
+        OpFoldResult iub;
+        auto cu = getConstantIntValue(u);
+        auto cs = getConstantIntValue(s);
+        if (cu && cs && (*cu % *cs == 0)) {
+          iub = s;
+        } else {
+          Value vub = cu ? rw.getConstant(*cu) : u.get<Value>();
+          Value vs = cs ? rw.getConstant(*cs) : s.get<Value>();
+          iub = OpFoldResult(rw.create<affine::AffineMinOp>(
+              idxType, minMap, ValueRange{vs, vub, i}));
+        }
+        innerBounds.emplace_back(iub);
+        ranges.emplace_back(Range{i, iub, one});
+      }
+    }
+
+    SmallVector<Value> innerOutputs;
+    for (auto o : outerLoop.getRegionIterArgs()) {
+      innerOutputs.emplace_back(rw.create<tensor::ExtractSliceOp>(o, ranges));
+    }
+
+    auto innerLoop =
+        rw.create<ForallOp>(SmallVector<OpFoldResult>(count, zero), innerBounds,
+                            innerSteps, innerOutputs, op.getMapping());
+    SmallVector<Type> argTypes{innerLoop.getBody()->getArgumentTypes()};
+    innerLoop.getRegion().takeBody(op.getRegion());
+    for (auto [arg, type] :
+         zip(innerLoop.getBody()->getArguments(), argTypes)) {
+      arg.setType(type);
+    }
+
+    // Collect all users of the inner loop outputs
+    llvm::SmallSet<Operation *, 4> outUsers;
+    for (auto out : innerLoop.getRegionIterArgs()) {
+      for (auto user : out.getUsers()) {
+        outUsers.insert(user);
+      }
+    }
+
+    // Replace the induction variables of the inner loop with the sum of the
+    // outer and inner induction variables, but only in the operations, that
+    // are not using the inner loop outputs, which are already sliced.
+    rw.setInsertionPointToStart(innerLoop.getBody());
+    for (auto [inIdx, outIdx] : llvm::zip(innerLoop.getInductionVars(),
+                                          outerLoop.getInductionVars())) {
+      auto newIdx = rw.create<arith::AddIOp>(inIdx, outIdx);
+      outUsers.insert(newIdx);
+      inIdx.replaceAllUsesExcept(newIdx, outUsers);
+    }
+
+    rw.setInsertionPointToStart(outerLoop.getTerminator().getBody());
+    for (auto [i, o] :
+         llvm::zip(innerLoop.getResults(), outerLoop.getRegionIterArgs())) {
+      rw.create<tensor::ParallelInsertSliceOp>(i, o, ranges);
+    }
+
+    rw.replaceOp(op, outerLoop);
   }
 };
 } // namespace
